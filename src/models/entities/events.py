4be1193b0f996524356c5909c71a69b4a23b12d5
Marcos Vinicius <marcosvtn433@gmail.com>
2024-04-05 00:06:36 -0300

    Addin try in events repository

--- src/models/entities/events.py
+++ src/models/entities/events.py
@@ -1,11 +1,15 @@
 from src.models.settings.base import base
-from sqlalchemy import Column,String,Integer
+from sqlalchemy import Column, String, Integer
 
-class Events(base): 
-    __tablename__= 'events'
+
+class Events(base):
+    __tablename__ = "events"
 
     id = Column(String, primary_key=True)
     title = Column(String, nullable=False)
     details = Column(String)
     slug = Column(String, nullable=False)
     maximum_attendees = Column(Integer)
+
+    def __repr__(self):
+        return f"Events [title={self.title}, maximum_attendees={self.maximum_attendees}]"
